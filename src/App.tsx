62b65d1ffc48e1d328edbebb2dc6efbc2b19ec89
Ayur Tushinov <tushinov.ayur@gmail.com>
2024-07-06 11:42:45 +0300

    feat: add cardlist and error boundary functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import Logo from "@/assets/logo.png";
-import HelloWorld from "@/components/HelloWorld/HelloWorld";
+import { Component } from "react";
 
-import styles from "./App.module.css";
+import BreakUiButton from "./components/breakUiButton/BreakUiButton";
+import CardList from "./components/cardlist/CardList";
+import Searchbar from "./components/searchbar/Searchbar";
 
-export default function App() {
-  return (
-    <main className={styles.main}>
-      <img className={styles.logo} alt="React logo" width="400px" src={Logo} />
-      <HelloWorld msg="Hello React + TypeScript + Vite" />
-    </main>
-  );
+interface IState {
+  text: string;
+}
+
+export default class App extends Component<object, IState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      text: "",
+    };
+  }
+
+  onType = (query: string) => {
+    this.setState({ text: query });
+  };
+
+  render() {
+    const { text } = this.state;
+    return (
+      <div className="app">
+        <Searchbar onType={this.onType} />
+        <CardList onSearch={text} />
+        <BreakUiButton />
+      </div>
+    );
+  }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import ErrorBoundary from "./components/errorBoundary/ErrorBoundary";
 
 const container = document.getElementById("root");
 const root = createRoot(container as HTMLDivElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
