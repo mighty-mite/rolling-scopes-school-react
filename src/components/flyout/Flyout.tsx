81d909fcf70475f6d3c79fe92a84395584de2fc0
Ayur Tushinov <tushinov.ayur@gmail.com>
2024-07-21 18:39:40 +0300

    feat: add flyout element layout

---
+++ src/components/flyout/Flyout.tsx
@@ -0,0 +1,11 @@
+function Flyout() {
+  return (
+    <div className="flyout">
+      <span className="flyout__header">Items selected</span>
+      <button className="flyout__unselect">Unselect all</button>
+      <button className="flyout__donwload">Donwload</button>
+    </div>
+  );
+}
+
+export default Flyout;
