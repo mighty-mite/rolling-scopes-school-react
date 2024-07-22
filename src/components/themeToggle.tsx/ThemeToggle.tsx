dd38a18b5195b90446c360b6489f3a008a9fed03
Ayur Tushinov <tushinov.ayur@gmail.com>
2024-07-22 16:28:35 +0300

    feat: add dark theme for theme toggler

--- src/components/themeToggle.tsx/ThemeToggle.tsx
+++ src/components/themeToggle.tsx/ThemeToggle.tsx
@@ -1,16 +1,25 @@
 import "./themeToggle.css";
 
+import { useContext } from "react";
+
+import ThemeContext from "@/themeContext/themeContext";
+
 interface IProps {
   changeTheme: (arg: string) => void;
 }
 
 function ThemeToggle(props: IProps) {
   const { changeTheme } = props;
+  const theme = useContext(ThemeContext);
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     changeTheme(e.target.value);
   };
   return (
-    <select className="theme-toggle" id="theme-toggle" onChange={handleChange}>
+    <select
+      className={`theme-toggle ${theme}`}
+      id="theme-toggle"
+      onChange={handleChange}
+    >
       <option value="light">Light theme</option>
       <option value="dark">Dark theme</option>
     </select>
