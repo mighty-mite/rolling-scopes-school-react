f28c78ce7ac980bf3e41ee05d8e0dbe0b270a00d
Ayur Tushinov <tushinov.ayur@gmail.com>
2024-07-22 21:18:44 +0300

    feat: hide flyout element when there is no selected items

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 
 import useLocalStorage from "@/hooks/useLocalStorage";
+import { RootState } from "@/store/store";
 
 import CardList from "../cardlist/CardList";
 import Flyout from "../flyout/Flyout";
@@ -11,6 +13,8 @@ function Layout() {
   const [storageVal] = useLocalStorage();
   const [searchTerm, setSearchTerm] = useState(storageVal ?? "");
 
+  const { selected } = useSelector((state: RootState) => state.flyout);
+
   const onType = (query: string) => {
     setSearchTerm(query);
   };
@@ -19,7 +23,7 @@ function Layout() {
       <section>
         <Searchbar onType={onType} />
         <CardList searchQuery={searchTerm} />
-        <Flyout />
+        {selected.length > 0 ? <Flyout /> : null}
       </section>
       <Outlet />
     </main>
