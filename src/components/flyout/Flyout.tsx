a6ddf0dd68fc3c1ae7109cf579b149661f64c013
Ayur Tushinov <tushinov.ayur@gmail.com>
2024-07-23 15:26:05 +0300

    feat: add download-csv-functionality

--- src/components/flyout/Flyout.tsx
+++ src/components/flyout/Flyout.tsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/store/store";
 import ThemeContext from "@/themeContext/themeContext";
 
+import convertToCSV from "../service/convertToCsv";
 import { unselectAll } from "./flyoutSlice";
 
 function Flyout() {
@@ -13,20 +14,33 @@ function Flyout() {
   const dispatch = useDispatch<AppDispatch>();
 
   const items = useSelector((state: RootState) => {
-    return state.flyout.selected.length;
+    return state.flyout.selected;
   });
 
   const handleUnselect = () => {
     dispatch(unselectAll());
   };
 
+  const downloadCSV = () => {
+    const csvData = new Blob([convertToCSV(items)], { type: "text/csv" });
+    const csvURL = URL.createObjectURL(csvData);
+    const link = document.createElement("a");
+    link.href = csvURL;
+    link.download = `${items.length}_items.csv`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className={`flyout ${theme}`}>
-      <span className="flyout__header">Items selected: {items}</span>
+      <span className="flyout__header">Items selected: {items.length}</span>
       <button className={`flyout__unselect ${theme}`} onClick={handleUnselect}>
         Unselect all
       </button>
-      <button className={`flyout__donwload ${theme}`}>Donwload</button>
+      <button className={`flyout__donwload ${theme}`} onClick={downloadCSV}>
+        Donwload
+      </button>
     </div>
   );
 }
